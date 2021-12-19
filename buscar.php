a3467bf89e346182a61d97686d5d4e51b4694187
raiesbo <raiesbo@gmail.com>
2021-12-19 23:02:55 +0100

    Add log-in and sign-up buttons to index page

--- buscar.php
+++ buscar.php
@@ -1,33 +1,33 @@
-<?php 
-if (!isset($_POST['cadena'])){ 
-      echo "Debe especificar una cadena a bucar"; 
-      exit; 
-} 
+<?php
+if (!isset($_POST['cadena'])) {
+      echo "Debe especificar una cadena a bucar";
+      exit;
+}
 $link = mysqli_connect("localhost", "root", "", "ejercicio7"); // es la contraseña;
-$result = mysqli_query($link, "SELECT * FROM datos WHERE nombre LIKE '%{$_POST['cadena']}%' ORDER BY nombre"); 
-if ($row = mysqli_fetch_array($result)){ 
-      echo "<table border = '1'> \n"; 
-//Mostramos los nombres de las tablas 
-echo "<tr> \n"; 
-while ($field = mysqli_fetch_field($result)){ 
-            echo "<td>$field->name</td> \n"; 
-} 
-      echo "</tr> \n"; 
-do { 
-            echo "<tr> \n"; 
-            echo "<td>".$row["id"]."</td> \n"; 
-            echo "<td>".$row["nombre"]."</td> \n"; 
-            echo "<td>".$row["apellido"]."</td> \n"; 
-            echo "<td>".$row["deporte"]."</td> \n"; 
-            echo "<td>".$row["sexo"]."</td> \n"; 
-			echo "<td>".$row["conducir"]."</td> \n";
-			echo "<td>".$row["aficiones"]."</td> \n";
+$result = mysqli_query($link, "SELECT * FROM datos WHERE nombre LIKE '%{$_POST['cadena']}%' ORDER BY nombre");
+if ($row = mysqli_fetch_array($result)) {
+      echo "<table border = '1'> \n";
+      //Mostramos los nombres de las tablas 
+      echo "<tr> \n";
+      while ($field = mysqli_fetch_field($result)) {
+            echo "<td>$field->name</td> \n";
+      }
+      echo "</tr> \n";
+      do {
+            echo "<tr> \n";
+            echo "<td>" . $row["id"] . "</td> \n";
+            echo "<td>" . $row["nombre"] . "</td> \n";
+            echo "<td>" . $row["apellido"] . "</td> \n";
+            echo "<td>" . $row["deporte"] . "</td> \n";
+            echo "<td>" . $row["sexo"] . "</td> \n";
+            echo "<td>" . $row["conducir"] . "</td> \n";
+            echo "<td>" . $row["aficiones"] . "</td> \n";
 
-            echo "</tr> \n"; 
-      } while ($row = mysqli_fetch_array($result)); 
-            echo "</table> \n"; 
-} else { 
-echo "¡ No se ha encontrado ningún registro !"; 
-} 
-?> 
-<p>Volver al inicio:    <a href="tarea8.html">  INICIO</a>
+            echo "</tr> \n";
+      } while ($row = mysqli_fetch_array($result));
+      echo "</table> \n";
+} else {
+      echo "¡ No se ha encontrado ningún registro !";
+}
+?>
+<p>Volver al inicio: <a href="tarea8.html"> INICIO</a>
--- insertar.php
+++ insertar.php
@@ -1,36 +1,24 @@
 <?php
 // Primero comprobamos que ningún campo esté vacío y que todos los campos existan.
-if(!isset($_POST['admin']))
-{
-	$_POST['admin']='No';
+if (!isset($_POST['admin'])) {
+	$_POST['admin'] = 'No';
 }
 
-if(isset($_POST['nombre']) && !empty($_POST['apellidos']) && isset($_POST['correo']) && !empty($_POST['contraseña'])&& isset($_POST['admin']))
-{
-// Si entramos es que todo se ha realizado correctamente y conectamos con la base de datos club de mysql
+if (isset($_POST['nombre']) && !empty($_POST['apellidos']) && isset($_POST['correo']) && !empty($_POST['contraseña']) && isset($_POST['admin'])) {
+	// Si entramos es que todo se ha realizado correctamente y conectamos con la base de datos club de mysql
+	$link = mysqli_connect("localhost", "root", "", "espasarc");
 
-//echo $name;
-// echo $surname;
-// echo $email;
-// echo $password;
+	// Con esta sentencia SQL insertaremos los datos en la base de datos
+	mysqli_query($link, "INSERT INTO datos(nombre,apellidos,correo,contraseña,admin)
+	VALUES ('{$_POST['nombre']}','{$_POST['apellidos']}','{$_POST['correo']}','{$_POST['contraseña']}','{$_POST['admin']}')");
 
-$link = mysqli_connect("localhost","root","","espasarc");
-
-// Con esta sentencia SQL insertaremos los datos en la base de datos
-mysqli_query($link,"INSERT INTO datos(nombre,apellidos,correo,contraseña,admin)
-VALUES ('{$_POST['nombre']}','{$_POST['apellidos']}','{$_POST['correo']}','{$_POST['contraseña']}','{$_POST['admin']}')");
-
-// Ahora comprobaremos que todo ha ido correctamente
-$my_error = mysqli_error($link);
-if (!empty($my_error)) {
-	echo "Ha habido un error al insertar los valores. $my_error";
+	// Ahora comprobaremos que todo ha ido correctamente
+	$my_error = mysqli_error($link);
+	if (!empty($my_error)) {
+		echo "Ha habido un error al insertar los valores. $my_error";
 	} else {
 		echo "Los datos han sido introducidos satisfactoriamente";
-		}
 	}
-else {
-echo "Error, no ha introducido todos los datos";
+} else {
+	echo "Error, no ha introducido todos los datos";
 }
-?>
-
-
